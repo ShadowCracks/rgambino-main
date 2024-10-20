a7c786833ebc89e25e5df0db1c72c3679c1f53d6
shadowcracks <yst187business@gmail.com>
2024-10-19 21:23:25 -0400

    new file:   components/blog/App.tsx 	new file:   components/blog/Footer.tsx 	new file:   components/blog/Header.tsx 	new file:   components/blog/Hero.tsx 	new file:   components/blog/Portfolio.tsx 	new file:   components/blog/PortfolioFilter.tsx 	new file:   components/blog/PortfolioGrid.tsx 	new file:   components/blog/PortfolioItem.tsx 	new file:   components/blog/WhatWeDo.tsx 	new file:   components/edit/App.tsx 	new file:   components/edit/Footer.tsx 	new file:   components/edit/Header.tsx 	new file:   components/edit/MainContent.tsx 	new file:   components/edit/PortfolioItem.tsx 	new file:   components/edit/PortfolioSection.tsx 	new file:   components/edit/WhatWeDoSection.tsx 	new file:   pages/blog.tsx 	new file:   pages/edit.tsx

---
+++ pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../components/blog/App'
+
+function blog () {
+  return (
+    <div>
+      <App />
+    </div>
+  )
+}
+
+export default blog
---
+++ pages/edit.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainContent from '../components/edit/MainContent'
+
+function edit() {
+  return (
+    <div>
+      <MainContent />
+    </div>
+  )
+}
+
+export default edit
