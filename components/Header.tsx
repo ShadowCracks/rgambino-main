e7bc95c6f03825ad1c6ce6660aa6dd339148355b
shadowcracks <yst187business@gmail.com>
2024-11-13 07:25:00 -0500

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Link from 'next/link';
 interface HeaderProps {
   navItems: string[];
 }
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = ({ navItems }) => {
   return (
     <header className="flex flex-col justify-center items-center px-5 py-3 w-full text-base text-white border border-black border-solid bg-neutral-900">
       <div className="flex flex-wrap gap-5 justify-between w-full max-w-full">
-        <a
+        <Link
           href="https://www.instagram.com/the_galactic_shine?igsh=MTQwbnA3bjA3YzE2ZQ%3D%3D" // Replace with your Instagram URL
           target="_blank"
           rel="noopener noreferrer"
@@ -19,7 +19,7 @@ const Header: React.FC<HeaderProps> = ({ navItems }) => {
             alt="Company logo"
             className="object-contain shrink-0 w-[71px] h-auto" // Use h-auto for responsive height
           />
-        </a>
+        </Link>
         <nav className="flex flex-wrap gap-10 items-start self-start mt-5">
           {navItems.map((item, index) => (
             <a key={index} href={item.replace(/\s+/g, '')}>{item}</a>
